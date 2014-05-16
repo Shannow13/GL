47f6eebb239927f53de8c5fbfa37f79b3fa75bfc
Shannow13 <pjmescle@gmail.com>
2014-05-16 11:01:51 +0200

    Ajout de la classe SimpleCarottes

---
+++ GL/src/game/SimpleCarottes.java
@@ -0,0 +1,34 @@
+package game;
+
+public class SimpleCarottes implements Carottes {
+	
+	private int positionX;
+	private int positionY;
+	private int nombre;
+	
+	public SimpleCarottes(int positionX, int positionY, int nombre){
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.nombre = nombre;
+	}
+
+	@Override
+	public int getPositionX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPositionY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNombre() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
