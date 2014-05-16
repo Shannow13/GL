c68e061b4f640a505197d746fb12e890a5c055e4
Shannow13 <pjmescle@gmail.com>
2014-05-16 14:53:47 +0200

    Ajout d'une classe AbstractJardinDaoTest pour faire les tests

---
+++ GL/src/game/AbstractJardinDaoTest.java
@@ -0,0 +1,13 @@
+package game;
+import org.apache.log4j.*;
+
+
+public abstract class AbstractJardinDaoTest {
+	 private static final Logger LOGGER = Logger.getLogger(AbstractJardinDaoTest.class);
+	 
+	 private final static String RESOURCES_PATH = "resources/";
+	 private final static String JARDIN_FILE_NAME = "jardin-1.csv";
+	 
+	 
+
+}
