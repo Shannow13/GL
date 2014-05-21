54e11aece20921d00cb27039de3334126f74d3ba
Aper0 <Aper0@10.8.95.96>
2014-05-21 14:14:52 +0200

    BidonJardinDao added, unimplemented

---
+++ GL/src/gl/dao/BidonJardinDao.java
@@ -0,0 +1,13 @@
+package gl.dao;
+
+import game.Jardin;
+
+public class BidonJardinDao implements JardinDao {
+
+	@Override
+	public Jardin findJardin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
