8764ed0949414738c605e86772bdc88bbd2de763
Windows <Windows@192.168.2.114>
2014-05-22 22:48:15 +0200

    Editor IHM

--- GL/src/editor/EditorIHM.java
+++ GL/src/editor/EditorIHM.java
@@ -3,6 +3,7 @@ package editor;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -10,7 +11,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import org.apache.log4j.Logger;
 
@@ -21,6 +24,7 @@ public class EditorIHM {
 	public JFrame frame;
 	//public JPanel panelG, panel;
 	public JButton jardin, carotte, rocher, renard;
+	public boolean flagAnnuler = false;
 	
 	public EditorIHM()
 	{
@@ -31,6 +35,7 @@ public class EditorIHM {
 		frame.setSize(new Dimension(500,500));
 		frame.setBounds(0, 0, 500, 500);
 		frame.setLayout(null);
+		frame.setLocation(600,300);
 		
 		jardin = new JButton("Jardin");
 		carotte = new JButton("Carotte");
@@ -83,19 +88,101 @@ public class EditorIHM {
 	
 	public class EditJardin implements ActionListener{
 		public void actionPerformed(ActionEvent e){
+			int w = (frame.getWidth()/2)-50;
+			int h = (frame.getHeight()/2)-150;
+			
 			LOGGER.debug("Click de jardin");
-			System.exit(0);
+			final JFrame frmJardin = new JFrame("Création Jardin");
+			//frmJardin.setUndecorated(true);
+			//frmJardin.setDefaultCloseOperation(EXIT_ON_CLOSE);
+			frmJardin.setSize(new Dimension(500,300));
+			frmJardin.setLocation(600, 300);
+			frmJardin.setLayout(null);
+			frmJardin.getContentPane().setBackground(Color.CYAN);
+			frmJardin.setResizable(false);
+			
+			JTextField colonnes = new JTextField("Nb de colonnes");
+			JTextField lignes = new JTextField("Nb de lignes");
+			JButton btnAnnuler = new JButton("Annuler");
+			JButton btnOk = new JButton("OK");
+			
+			frmJardin.add(colonnes);
+			frmJardin.add(lignes);
+			frmJardin.add(btnAnnuler);
+			frmJardin.add(btnOk);
+			
+			colonnes.setBounds( w - 125, h - 50, 150, 40);
+		    lignes.setBounds( w + 75, h - 50, 150, 40);
+		    btnAnnuler.setBounds( w - 125, h + 50, 150, 40);
+		    btnOk.setBounds( w + 75, h + 50, 150, 40 );
+		    
+		    btnAnnuler.addActionListener(new ActionListener()
+            {
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        frmJardin.setVisible( false );
+                    }
+            });			
+		    
+		    btnOk.addActionListener(new ActionListener()
+            {
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        frmJardin.setVisible( false );
+                    }
+            });	
+		    
+			frmJardin.setVisible(true);
 		}
 	}
 	
 	public class EditCarotte implements ActionListener{
 		public void actionPerformed(ActionEvent e){
+			int w = (frame.getWidth()/2)-200;
+			int h = (frame.getHeight()/2)-200;
+			
 			LOGGER.debug("Click de carotte");
-			System.exit(0);
+			final JFrame frmNbC = new JFrame("Nombre de Carottes");
+			frmNbC.setSize(new Dimension(300,300));
+			frmNbC.setLocation(700, 300);
+			frmNbC.setLayout(null);
+			frmNbC.getContentPane().setBackground(new Color(255, 102, 0));
+			frmNbC.setResizable(false);
+			
+			JLabel text = new JLabel("Entrez le nombre de carottes:");
+			JTextField nbCarottes = new JTextField("");
+			JButton btnOk = new JButton("OK");
+			JButton btnAnnuler = new JButton("Annuler");
+			
+			frmNbC.add(text);
+			frmNbC.add(nbCarottes);
+			frmNbC.add(btnOk);
+			frmNbC.add(btnAnnuler);
+			
+			text.setBounds( w + 15, h - 30, 200, 40);
+		    nbCarottes.setBounds( w + 15, h + 20, 160, 40);
+			btnOk.setBounds( w + 15, h + 100, 160, 40);
+			btnAnnuler.setBounds( w + 15, h + 150, 160, 40);
+			
+			btnAnnuler.addActionListener(new ActionListener()
+            {
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        frmNbC.setVisible( false );
+                    }
+            });			
+		    
+		    btnOk.addActionListener(new ActionListener()
+            {
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        frmNbC.setVisible( false );
+                    }
+            });	
+		    
+			frmNbC.setVisible(true);
 		}
-	}
-	
-	public class EditRocher implements ActionListener{
+	} class EditRocher implements ActionListener{
 		public void actionPerformed(ActionEvent e){
 			LOGGER.debug("Click de rocher");
 			System.exit(0);
@@ -108,6 +195,4 @@ public class EditorIHM {
 			System.exit(0);
 		}
 	}
-	
 }
-
