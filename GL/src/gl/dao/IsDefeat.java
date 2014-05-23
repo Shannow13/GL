e4860f92604a847440b9238435df90389a6fa20c
Triskell <Triskell@Triskell-PC.home>
2014-05-23 23:17:47 +0200

    Fenetres Victoire + Defaite

--- GL/src/gl/dao/IsDefeat.java
+++ GL/src/gl/dao/IsDefeat.java
@@ -2,6 +2,8 @@ package gl.dao;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -28,24 +30,42 @@ public class IsDefeat extends JFrame {
     }
 	*/
 	public IsDefeat() {
-		//this.flagWin = flagWin;
 		
-		JFrame frmWin = new JFrame("Défaite !");
+		final JFrame frmWin = new JFrame("Défaite !");
 		
 		frmWin.setSize(new Dimension(500,500));
 		frmWin.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frmWin.setLocation(700, 300);
 		
 		JPanel panWin = new JPanel();
 		
 		JButton btnQuit = new JButton("RageQuit");
 		JButton btnReload = new JButton("Rejouer");
 		
+		btnReload.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+            	frmWin.setVisible(false);
+            	frmWin.dispose();
+            }
+        });
+		
+		btnQuit.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+            	frmWin.setVisible(false);
+            	frmWin.dispose();
+            }
+        });
+		
 		panWin.add(btnQuit);
 		panWin.add(btnReload);
 		
 		frmWin.add(panWin);
 		
-		JLabel imgWin = new JLabel(new ImageIcon("resources/img/faux3.png"));
+		JLabel imgWin = new JLabel(new ImageIcon("resources/img/defeat.png"));
 		imgWin.setPreferredSize(new Dimension(100, 100));
 		frmWin.add(imgWin, BorderLayout.CENTER);
 		frmWin.add(panWin, BorderLayout.SOUTH);
--- GL/src/gl/dao/IsVictory.java
+++ GL/src/gl/dao/IsVictory.java
@@ -1,6 +1,10 @@
 package gl.dao;
 
+import gl.dao.Tableau.Avance;
+
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Dimension;
 
 import javax.swing.ImageIcon;
@@ -28,22 +32,53 @@ public class IsVictory extends JFrame {
     }
 	*/
 	public IsVictory() {
-JFrame frmWin = new JFrame("Victoire !");
+		
+		final JFrame frmWin = new JFrame("Victoire !");
 		
 		frmWin.setSize(new Dimension(500,500));
 		frmWin.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frmWin.setLocation(700, 300);
 		
 		JPanel panWin = new JPanel();
 		
 		JButton btnNext = new JButton("Continuer");
 		JButton btnReload = new JButton("Rejouer");
+		JButton btnMenu = new JButton("Menu");
+		
+		btnNext.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+            	frmWin.setVisible(false);
+            	frmWin.dispose();
+            }
+        });
+		
+		btnReload.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+            	frmWin.setVisible(false);
+            	frmWin.dispose();
+            }
+        });
+		
+		btnMenu.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+            	frmWin.setVisible(false);
+            	frmWin.dispose();
+            }
+        });
 		
 		panWin.add(btnNext);
 		panWin.add(btnReload);
+		panWin.add(btnMenu);
 		
 		frmWin.add(panWin);
 		
-		JLabel imgWin = new JLabel(new ImageIcon("resources/img/bugs_bunny.jpg"));
+		JLabel imgWin = new JLabel(new ImageIcon("resources/img/victoire.png"));
 		imgWin.setPreferredSize(new Dimension(100, 100));
 		frmWin.add(imgWin, BorderLayout.CENTER);
 		frmWin.add(panWin, BorderLayout.SOUTH);
@@ -53,4 +88,5 @@ JFrame frmWin = new JFrame("Victoire !");
 		//this.flagWin = flagWin;
 		
 	}
+	
 }
