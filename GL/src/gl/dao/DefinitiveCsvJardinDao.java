bfcad036f10f385a0bdbe4468bfca51d1cc8848f
Aper0 <Aper0@192.168.2.102>
2014-05-23 12:38:00 +0200

    Correction CSVJardinDao

--- GL/src/gl/dao/DefinitiveCsvJardinDao.java
+++ GL/src/gl/dao/DefinitiveCsvJardinDao.java
@@ -17,44 +17,44 @@ import org.apache.log4j.Logger;
 import au.com.bytecode.opencsv.CSVReader;
 
 public class DefinitiveCsvJardinDao implements CsvJardinDao {
-	
+
 	private File file;
 	private static final Logger LOGGER = Logger.getLogger(DefinitiveCsvJardinDao.class);
 	private final static char SEPARATOR = ';';
 	private SimpleJardin lvljardin;
 	private ArrayList<Carottes> carottes;
 	private ArrayList<Rocher> rochers;
-	
-	
+
+
 	@Override
 	public void init(File file) {
 		LOGGER.debug("Initialisation du fichier");
 		this.file=file;
-		
+
 		//Nouvelle lecture à chaque init. Cela permet ainsi de changer de fichier ou de le recharger.
 		reloadJardin();
-		
-	}
-	
-	
-	
-	private void reloadJardin() {
-	
-		LOGGER.debug("reloadJardin");
-	
 
-        if (file == null) {
-            throw new IllegalStateException("Le fichier est vide...");
-       
-        }try{
-        	//On construit le Jardin
-        	
-        	lvljardin = new SimpleJardin();
-        	//Transformation du CSV en liste de String[] afin de créer un jardin correct
-		final List<String[] > lignes = getLignesFromFile();
-		
+	}
+
+
+
+	private void reloadJardin() {
+
+		LOGGER.debug("reloadJardin");
+
+
+		if (file == null) {
+			throw new IllegalStateException("Le fichier est vide...");
+
+		}try{
+			//On construit le Jardin
+
+			lvljardin = new SimpleJardin();
+			//Transformation du CSV en liste de String[] afin de créer un jardin correct
+			final List<String[] > lignes = getLignesFromFile();
+
 			LOGGER.debug("On a transformé le CSV en lignes");
-		
+
 			for(String[] ligne : lignes){
 				if(ligne[0].charAt(0) == 'J'){
 					LOGGER.debug("Jardin");
@@ -73,17 +73,17 @@ public class DefinitiveCsvJardinDao implements CsvJardinDao {
 					LOGGER.debug("Rocher ajouté !");
 				}
 			}
-		
+
 			//on transforme le tout en jardin
 			transformLigneToJardin(lignes.get(0), lvljardin, carottes, rochers);
-		
-        }catch(Exception e){
-        	LOGGER.error("fuck");
-        }
+
+		}catch(Exception e){
+			LOGGER.error("fuck");
+		}
 	}
 
 	private ArrayList<Carottes> addCarottes(String[] ligne, ArrayList<Carottes> carotte){
-		
+
 		//On crée un nouvel objet carotte
 		final SimpleCarottes carotteplus = new SimpleCarottes();
 		LOGGER.debug("Début de la création");
@@ -91,113 +91,112 @@ public class DefinitiveCsvJardinDao implements CsvJardinDao {
 		carotteplus.setPositionX(Integer.parseInt(ligne[1]));
 		carotteplus.setPositionY(Integer.parseInt(ligne[2]));
 		carotteplus.setNombre(Integer.parseInt(ligne[3]));
-		
+
 		LOGGER.debug("ajout à la liste");
-		if(carottes == null)
+		if(this.carottes == null)
 		{
 			//Si la liste passée en paramètre est nulle on la crée
-			carottes = new ArrayList<Carottes>();
+			this.carottes = new ArrayList<Carottes>();
 			LOGGER.debug("carottes NULL");
 		}
-			//On ajoute l'objet à la liste
-			carottes.add(carotteplus);
-			LOGGER.debug("A été ajouté");
-			// on retourne la Liste
-	return carotte;
+		//On ajoute l'objet à la liste
+		this.carottes.add(carotteplus);
+		LOGGER.debug("A été ajouté");
+		// on retourne la Liste
+		return this.carottes;
 	}
 
-private ArrayList<Rocher> addRochers(String[] ligne, ArrayList<Rocher> rochers){
-	//Même principe que pour la carotte
-	final SimpleRocher rocherplus = new SimpleRocher();
-	
-	LOGGER.debug("Création de l'objet Rocher");
-	rocherplus.setPositionX(Integer.parseInt(ligne[1]));
-	rocherplus.setPositionY(Integer.parseInt(ligne[2]));
-	
-	LOGGER.debug("Ajout à la liste");
-	if(rochers == null){
-		
-		rochers = new ArrayList<Rocher>();
-		LOGGER.debug("rochers NULL");
+	private ArrayList<Rocher> addRochers(String[] ligne, ArrayList<Rocher> rochers){
+		//Même principe que pour la carotte
+		final SimpleRocher rocherplus = new SimpleRocher();
+
+		LOGGER.debug("Création de l'objet Rocher");
+		rocherplus.setPositionX(Integer.parseInt(ligne[1]));
+		rocherplus.setPositionY(Integer.parseInt(ligne[2]));
+
+		LOGGER.debug("Ajout à la liste");
+		if(this.rochers == null){
+
+			this.rochers = new ArrayList<Rocher>();
+			LOGGER.debug("rochers NULL");
+		}
+		this.rochers.add(rocherplus);
+		LOGGER.debug("A été ajouté");
+
+		return this.rochers;
 	}
-	rochers.add(rocherplus);
-	LOGGER.debug("A été ajouté");
-	
-	return rochers;
-}
 
 
 
 	private List<String[]> getLignesFromFile(){
-	
-			LOGGER.debug("getLignesFromFile");
 
-	        if (file == null) {
-	            throw new IllegalStateException("Le fichier est vide...");
-	        }
+		LOGGER.debug("getLignesFromFile");
 
-	        final List<String[] > lignes = new ArrayList<String[] >();
-	        
-	        try {
-	        	// on ouvre le fichier et on se sert de csvreader afin de le transformer en liste de String[]
-	            final FileReader fr = new FileReader(file);
-	            final CSVReader csvReader = new CSVReader(fr, SEPARATOR);
-	            
-	            
-	            
-	            String[] nextLine = null;
-	            while ((nextLine = csvReader.readNext()) != null) {
-	                int size = nextLine.length;
+		if (file == null) {
+			throw new IllegalStateException("Le fichier est vide...");
+		}
 
-	                // ligne vide
-	                if (size == 0) {
-	                    continue;
-	                }
-	                String debut = nextLine[0].trim();
-	                if (debut.isEmpty() && size == 1) {
-	                    continue;
-	                }
+		final List<String[] > lignes = new ArrayList<String[] >();
 
-	                // ligne de commentaire
-	                if (debut.startsWith("#")) {
-	                    continue;
-	                }
-	                lignes.add(nextLine);
-	            }
-	            csvReader.close();
-	        } catch (Exception e) {
-	            LOGGER.error("ça marche pô...", e);
-	        }
-	      
-	        return lignes;
-	    }
+		try {
+			// on ouvre le fichier et on se sert de csvreader afin de le transformer en liste de String[]
+			final FileReader fr = new FileReader(file);
+			final CSVReader csvReader = new CSVReader(fr, SEPARATOR);
 
 
-    private Jardin transformLigneToJardin(final String[] lignes, final SimpleJardin jardin, final ArrayList<Carottes> carottes, final ArrayList<Rocher> rochers){
-    	//On crée le Jardin
-    		
-    		jardin.setSizeX(Integer.parseInt(lignes[1]));
-    		jardin.setSizeY(Integer.parseInt(lignes[2]));
-    		jardin.setIndicelvl(Integer.parseInt(lignes[3]));
-    		jardin.setDebloque(Integer.parseInt(lignes[4]));
-    		jardin.setCarottes(carottes);
-    		jardin.setRochers(rochers);
-    		System.out.println(jardin.sizeX);
-    		System.out.println(jardin.sizeY);
-    		
+
+			String[] nextLine = null;
+			while ((nextLine = csvReader.readNext()) != null) {
+				int size = nextLine.length;
+
+				// ligne vide
+				if (size == 0) {
+					continue;
+				}
+				String debut = nextLine[0].trim();
+				if (debut.isEmpty() && size == 1) {
+					continue;
+				}
+
+				// ligne de commentaire
+				if (debut.startsWith("#")) {
+					continue;
+				}
+				lignes.add(nextLine);
+			}
+			csvReader.close();
+		} catch (Exception e) {
+			LOGGER.error("ça marche pô...", e);
+		}
+
+		return lignes;
+	}
+
+
+	private Jardin transformLigneToJardin(final String[] lignes, final SimpleJardin jardin, final ArrayList<Carottes> carottes, final ArrayList<Rocher> rochers){
+		//On crée le Jardin
+
+		jardin.setSizeX(Integer.parseInt(lignes[1]));
+		jardin.setSizeY(Integer.parseInt(lignes[2]));
+		jardin.setIndicelvl(Integer.parseInt(lignes[3]));
+		jardin.setDebloque(Integer.parseInt(lignes[4]));
+		jardin.setCarottes(carottes);
+		jardin.setRochers(rochers);
+		System.out.println(jardin.sizeX);
+		System.out.println(jardin.sizeY);
+
 		return jardin;
-    }
-    	
-  
+	}
+
+
 
 
 	@Override
 	public Jardin findJardin() {
 		LOGGER.debug("Début du remplissage du Jardin");
-		
-		
-		
+
+
+
 		return lvljardin;
 	}
 }
-	
